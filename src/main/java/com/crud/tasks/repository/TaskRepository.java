7612d738ecd3b7ff0ad58e01d5d141ae6a4947ab
Luke1024 <chajdas.lukasz@gmail.com>
2019-04-18 11:16:30 +0200

    task 19.3

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -5,10 +5,18 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
-    Task findById(Long id);
+
+    @Override
+    Task save(Task task);
+
+    Optional<Task> findById(Long id);
+
+    Optional<Task> deleteById(Long id);
+
 }
